ae0ade773dc023aaf9d58c0b040ae70a7fe3fdec
Srishti Agarwal <agarwal11srishti@gmail.com>
2025-01-11 19:30:30 +0530

    Integration: Rainbow's Facebook Ads

--- backend/src/utils/adDimensions.js
+++ backend/src/utils/adDimensions.js
@@ -5,6 +5,6 @@ export const adDimensionsConfig = [
     { width: 1080, height: 1080, name: "1080x1080" },
     // { width: 1440, height: 1440, name: "1440x1440" },
     // { width: 1440, height: 1800, name: "1440x1800" },
-    // { width: 1440, height: 2560, name: "1440x2560" },
+    { width: 1440, height: 2560, name: "1440x2560" },
     // { width: 728, height: 90, name: "728x90" },
 ];
